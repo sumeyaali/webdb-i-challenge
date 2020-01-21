02807a33189f5c6f6c5e5917ae6e0fcedf9afdde
sumeyaali <thesumeyaali@gmail.com>
2020-01-21 13:44:53 -0800

    not complete yet

--- queries.sql
+++ queries.sql
@@ -2,15 +2,42 @@
 
 -- Find all customers with postal code 1010
 
+SELECT * FROM [Customers]
+where PostalCode = 1010
+
+
 -- Find the phone number for the supplier with the id 11
+
+SELECT Phone 
+from suppliers
+where SupplierID = 11
 
 -- List first 10 orders placed, sorted descending by the order date
 
+select * 
+from Orders
+order by OrderDate desc
+limit 10 
+
+
 -- Find all customers that live in London, Madrid, or Brazil
+
+SELECT City, Country
+from Customers
+where city in ('London','Madrid') or country = 'Brazil'
+
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 
+insert into Customers (CustomerName,ContactName, Address, City, PostalCode, Country)
+values ('The Shire','Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', 111,'Middle Earth')
+
+
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+
+update Customers 
+set PostalCode = 11122
+where CustomerID = 93
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 
 const db = require('./data/dbConfig.js');
-
+const Router = require('./data/seeds/accountRouter');
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', Router)
+
+server.get('/', (req, res) => {
+    res.send(`<h2>IT'S WORKING! IT'S WORKING!</h2>`)
+})
 
 module.exports = server;
